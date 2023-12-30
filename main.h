a5e3fe4c7ceaccaa765f5d60d03498051e3d8650
TheBMA <benrochd.m.ali@gmail.com>
2023-12-29 16:41:53 -0800

    Cash stands for Chaimaa & Ali s Shell

---
+++ main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*libraries based on the allowed functions*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <dirent.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+/*more function prototypes coming soon..*/
+void display_prompt(void);
+char *take_command(void);
+void execute_command(char *command);
+
+#endif
